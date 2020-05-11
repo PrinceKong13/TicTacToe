3c1b1cded1dc3c091d52a2d2954ea578c47bbd68
Will <wk1394@gmail.com>
2020-05-11 15:05:52 -0500

    Game now recognizes a win and a draw

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let cells = document.querySelectorAll(".row > div");
 let lastPlayerMove = "O";
-let winner = null;
 let winnerDisplay = document.querySelector("#board > h2");
+let numOfClicks = 0;
 
 for (let i = 0; i < cells.length; i++) {
   cells[i].addEventListener("click", cellClicked);
@@ -12,10 +12,12 @@ function cellClicked() {
   if (lastPlayerMove === "O") {
     event.target.textContent = "X";
     lastPlayerMove = "X";
+    ++numOfClicks;
     checkWinner();
   } else {
     event.target.textContent = "O";
     lastPlayerMove = "O";
+    ++numOfClicks;
     checkWinner();
   }
 }
@@ -23,19 +25,43 @@ function cellClicked() {
 //checks to see if there is a winner after each click and displays a message if so
 function checkWinner() {
   if (
-    cells[0].textContent === "X" &&
-    cells[1].textContent === "X" &&
-    cells[2].textContent === "X"
+    (cells[0].textContent === lastPlayerMove &&
+      cells[1].textContent === lastPlayerMove &&
+      cells[2].textContent === lastPlayerMove) ||
+    (cells[3].textContent === lastPlayerMove &&
+      cells[4].textContent === lastPlayerMove &&
+      cells[5].textContent === lastPlayerMove) ||
+    (cells[6].textContent === lastPlayerMove &&
+      cells[7].textContent === lastPlayerMove &&
+      cells[8].textContent === lastPlayerMove) ||
+    (cells[0].textContent === lastPlayerMove &&
+      cells[3].textContent === lastPlayerMove &&
+      cells[6].textContent === lastPlayerMove) ||
+    (cells[1].textContent === lastPlayerMove &&
+      cells[4].textContent === lastPlayerMove &&
+      cells[7].textContent === lastPlayerMove) ||
+    (cells[2].textContent === lastPlayerMove &&
+      cells[5].textContent === lastPlayerMove &&
+      cells[8].textContent === lastPlayerMove) ||
+    (cells[0].textContent === lastPlayerMove &&
+      cells[4].textContent === lastPlayerMove &&
+      cells[8].textContent === lastPlayerMove) ||
+    (cells[6].textContent === lastPlayerMove &&
+      cells[4].textContent === lastPlayerMove &&
+      cells[2].textContent === lastPlayerMove)
   ) {
-    winnerDisplay.textContent = "X Wins!";
-    console.log("X Wins");
+    winnerDisplay.textContent = lastPlayerMove + " Wins!";
+    console.log(lastPlayerMove + " Wins");
+    
+  } else if (numOfClicks === 9) {
+    console.log("Draw");
   }
 }
 
-//resets the board after a winner is declared
+//resets the board
 function resetBoard() {
   lastPlayerMove = "O";
   for (let i = 0; i < cells.length; i++) {
-    cells[i].textContent = " ";
+    cells[i].textContent = "";
   }
 }
